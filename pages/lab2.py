014cd69f939c375e1260a2093ed4f0132ed03f15
한양남자 <110797599+myoh0623@users.noreply.github.com>
2023-02-03 13:55:54 +0900

    Update and rename lab1.py to lab2.py
    slider bar

---
+++ pages/lab2.py
@@ -0,0 +1,8 @@
+from streamlit as st
+from datetime import datetime
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm")
+
+st.write("Start time:", start_time)
