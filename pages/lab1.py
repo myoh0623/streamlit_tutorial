af06f86d1df49bb3976fd2b6609f1dc0ad7f99aa
myoh0623 <myoh0623@gmail.com>
2023-02-03 13:43:18 +0900

    my second app

---
+++ pages/lab1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+# import cv2
+
+st.title("lab1")
+
+file = st.file_uploader("upload_file")
+
+if file is None:
+    file = st.camera_input("take picture")
+
+st.download_button(" 사진을 다운로드 ", file)
